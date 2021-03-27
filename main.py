d1723f76e7fb33b764178dc915301eb4852e2447
Francis Tseng <f+accounts@frnsys.com>
2021-03-27 10:07:48 -0400

    minor cleanup

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import re
 import random
 
-terms = {}
-for l in open('terms.txt', 'r').read().splitlines():
-    if not l: continue
-    if l.startswith('#'):
-        key = l.strip('# ')
-        terms[key] = []
-    else:
-        terms[key].append(l)
 
 def term_root(term):
     return term.split('|')[0] if '|' in term else term
 
-def fill_pattern(pattern, roots=None):
+def fill_pattern(pattern, terms, roots=None):
     roots = roots or []
     for k in re.findall('{[a-z]+}', pattern):
         key = k[1:-1]
@@ -22,10 +14,22 @@ def fill_pattern(pattern, roots=None):
             opts = terms[key]
         term = random.choice(opts)
         roots.append(term_root(term))
-        term = fill_pattern(term.replace('|', ''), roots)
+        term = term.replace('|', '') # remove root demarcation
+        term = fill_pattern(term, terms, roots)
         pattern = pattern.replace(k, term, 1)
     return pattern
 
-starts = terms.pop('start')
-for _ in range(10):
-    print(fill_pattern(random.choice(starts)))
+
+if __name__ == '__main__':
+    terms = {}
+    for l in open('terms.txt', 'r').read().splitlines():
+        if not l: continue
+        if l.startswith('#'):
+            key = l.strip('# ')
+            terms[key] = []
+        else:
+            terms[key].append(l)
+
+    starts = terms.pop('start')
+    for _ in range(10):
+        print(fill_pattern(random.choice(starts), terms))
